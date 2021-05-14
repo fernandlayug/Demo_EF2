df5d827827d89e415433dface390550aebb1855e
fernandlayug <fernand_layug@live.com>
2021-05-14 11:33:46 +0800

    Add project files.

---
+++ Demo_EF2/Services/DatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Demo_EF2.Model;
+using System.IO;
+
+
+
+
+namespace Demo_EF2.Services
+{
+    public class DatabaseContext : DbContext
+
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+        public DatabaseContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Employee.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+
+            
+    }
+}
---
+++ Demo_EF2/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Demo_EF2.Model;
+
+
+namespace Demo_EF2.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+
+        public async Task<List<EmployeeModel>>GetAllEmployees()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Employees.ToListAsync();
+            return res;
+
+        }
+        public
+    }
+}
